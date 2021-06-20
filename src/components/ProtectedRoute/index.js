b08bf98483470a43a833890568a33f2a6957bdb1
Phani <phani@ib005.local>
2021-06-20 20:33:10 +0530

    close out

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -3,6 +3,9 @@ import Cookies from 'js-cookie'
 
 const ProtectedRoute = props => {
   const token = Cookies.get('jwt_token')
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
   return <Route {...props} />
 }
 
